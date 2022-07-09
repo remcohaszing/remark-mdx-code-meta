c34f1dfc6e9359e00641bf552757168321620de5
Remco Haszing <remcohaszing@gmail.com>
2022-07-09 16:06:08 +0200

    Convert to ESM

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Parser } from 'acorn';
+import jsx from 'acorn-jsx';
+import { BaseNode, Program } from 'estree';
+import { Code, Parent, Root } from 'mdast';
+import { Plugin, Transformer } from 'unified';
+import { visit } from 'unist-util-visit';
+
+const parser = Parser.extend(jsx());
+
+const transformer: Transformer<Root> = (ast) => {
+  visit(ast, 'code', (node: Code, index: number | null, parent: Parent | null) => {
+    if (!node.meta) {
+      return;
+    }
+    const code = JSON.stringify(`${node.value}\n`);
+    const codeProps = node.lang ? `className="language-${node.lang}"` : '';
+    const value = `<pre ${node.meta}><code ${codeProps}>{${code}}</code></pre>`;
+    const estree = parser.parse(value, { ecmaVersion: 'latest' }) as BaseNode as Program;
+    parent!.children[index!] = {
+      type: 'mdxFlowExpression',
+      value,
+      data: { estree },
+    };
+  });
+};
+
+/**
+ * A markdown plugin for transforming code metadata.
+ *
+ * @returns A unified transformer.
+ */
+export const remarkMdxCodeMeta: Plugin<[], Root> = () => transformer;
--- test.ts
+++ test.ts
@@ -1,18 +1,18 @@
-import { promises as fs, readdirSync } from 'fs';
-import { createRequire } from 'module';
+import { readdir, readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 
 import { compile } from '@mdx-js/mdx';
 import prettier from 'prettier';
-import test from 'tape';
 import { toVFile } from 'to-vfile';
+import { test } from 'uvu';
+import { equal } from 'uvu/assert';
 
-const { remarkMdxCodeMeta } = createRequire(import.meta.url)('./src/index.ts');
+import { remarkMdxCodeMeta } from './index.js';
 
-const tests = readdirSync('__fixtures__');
+const tests = await readdir('__fixtures__');
 
 for (const name of tests) {
-  test(name, async (t) => {
+  test(name, async () => {
     const path = join('__fixtures__', name);
     let input;
     try {
@@ -21,15 +21,16 @@ for (const name of tests) {
       input = await toVFile.read(join(path, 'input.md'));
     }
     const expected = join(path, 'expected.jsx');
-    const { value } = await compile(input, {
+    const result = await compile(input, {
       remarkPlugins: [remarkMdxCodeMeta],
       jsx: true,
     });
-    const formatted = prettier.format(value, { parser: 'babel' });
+    const formatted = prettier.format(String(result), { parser: 'babel' });
     if (process.argv.includes('--write')) {
-      await fs.writeFile(expected, formatted);
+      await writeFile(expected, formatted);
     }
-    t.equal(formatted, await fs.readFile(expected, 'utf8'));
-    t.end();
+    equal(formatted, await readFile(expected, 'utf8'));
   });
 }
+
+test.run();
